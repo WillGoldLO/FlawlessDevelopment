e0fcbf3e223f3825e8952ef163d181f223f12ba0
Will Davis <45822339+mustangzrule@users.noreply.github.com>
2020-02-26 22:49:25 -0500

    First draft

---
+++ MainWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MainWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
